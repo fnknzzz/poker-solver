433e326efdb3ffed108f505cb1e0fede44462991
fnknzzz <fengkun@bytedance.com>
2021-06-12 06:39:20 +0800

    chore: remove warning

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ export const App = () => {
   useEffect(() => {
     init().then(() => {
       window.player = AiPlayer.new('778899JQQQKKK2', 'TTAA')
+      console.log(window.player.winner === Winner.Second)
     })
   }, [])
   return <div>ff</div>
