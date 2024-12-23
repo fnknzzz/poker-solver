4b0bef29fb8184981db6ec76de541eb701676271
fengkun <fengkun@bytedance.com>
2024-12-23 20:35:37 +0800

    fix: actions

--- poker-solver/benches/poker_benchmark.rs
+++ poker-solver/benches/poker_benchmark.rs
@@ -1,16 +1,11 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use poker_solver::calculate;
 
 pub fn poker_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("poker_solver");
-    group.bench_function("complex case", |b| {
-        b.iter(|| calculate(black_box("778899JQQQKKK2"), black_box("TTAA")))
+    c.bench_function("poker_solver", |b| {
+        b.iter(|| calculate("778899JQQQKKK2", "TTAA"))
     });
-    group.bench_function("simple case", |b| {
-        b.iter(|| calculate(black_box("55"), black_box("66")))
-    });
-    group.finish();
 }
 
-criterion_group!(poker_benches, poker_benchmark);
-criterion_main!(poker_benches);
+criterion_group!(benches, poker_benchmark);
+criterion_main!(benches);
